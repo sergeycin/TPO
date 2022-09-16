03d163c82d02755cfb4259fb0e957a1a6b142828
sergeycin <67867645+sergeycin@users.noreply.github.com>
2022-09-16 12:05:56 +0300

    TPO3

--- laba3.js
+++ laba3.js
@@ -1,13 +1,12 @@
 var fs = require('fs');
 
 class TPO {
-    // constructor(firstName, lastName) {
-    //     this.firstName = firstName
-    //     this.lastName = lastName
-    // }
 
+// MARK: FIRST TASK
+// Rotate by 90 * k degrees function
    rotate(matrix, k)  {
-        let isSquare = isSquareMatrix(matrix)
+      if(k <0 ) return 'k is wrong'
+        let isSquare = this.isSquareMatrix(matrix)
         if (isSquare == 'neverno') return 'matrix is no square'
         
         var result = [...matrix];
@@ -26,4 +25,64 @@ class TPO {
     
         return result
     }
+
+
+     isSquareMatrix(matrix) {
+      for(let item in matrix){
+              if(matrix[item].length != matrix[Number(item + 1)].length){
+                  return 'neverno'
+               }      
+      }
+  }
+
+// MARK: SECOND TASK
+
+ is5CharactersE(stroka) {
+  if(stroka.includes("eeeee")){
+      return true
+  }
+  else{
+      return false
+  }
+  }
+
+// MARK: THREE TASK
+   readFile(name){
+    var data = fs.readFileSync(name, 'utf8');
+    let count = 0;
+    for(let stroka of data.split('\n')){
+        let curStroke = String(stroka);
+  
+        if(curStroke.length <= 1) count++;
+        
+    }
+   
+        return count
+    
 }
+
+}
+
+
+const Test = new TPO()
+// Один тест на количество
+let matrix = [
+  [0,0,2],
+  [0,1]
+
+]
+let k = -4;
+let rotated = Test.rotate(matrix, k)
+console.log(`Rotateded matrix`, rotated)
+
+
+
+
+// В начале, в конце, в середине, присутствуют не все e, отсутствуют e
+let str = 'i tut'
+console.log('Is have 5 e', Test.is5CharactersE(str) )
+
+
+
+// В начале, в конце, в середине, присутствуют не все e, отсутствуют e
+console.log('Count space string',Test.readFile('file.txt'))
